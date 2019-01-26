94299883b0b2ec291da7a60ecd40054005427eeb
farinajf@gmail.com <farinajf@gmail.com>
2019-01-26 19:50:32 +0100

    flyway

--- flyway/execute.sh
+++ flyway/execute.sh
@@ -9,3 +9,10 @@
 # 3.- Ejecutar flyway info
 docker run --link flyway-mysql:flyway-mysql -v /home/fran/docker/dockerlab/flyway/conf:/flyway/conf my-flyway info
 
+# 4.- Ejecutar flyway baseline
+docker run --link flyway-mysql:flyway-mysql \
+	-v /home/fran/docker/dockerlab/flyway/sqldir:/lyway/sql \
+        -v /home/fran/docker/dockerlab/flyway/conf:/flyway/conf \
+	my-flyway \
+	baseline
+
