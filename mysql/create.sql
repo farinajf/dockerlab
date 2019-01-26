63e41a05d4304d37b421e8291676bb9842866ddd
farinajf@gmail.com <farinajf@gmail.com>
2019-01-26 18:19:37 +0100

    flyway

--- mysql/create.sql
+++ mysql/create.sql
@@ -3,7 +3,8 @@ CREATE TABLE empleados (id int AUTO_INCREMENT,
 	apellido varchar(25),
 	email varchar(50),
 	ciudad varchar(50),
-	pais varchar(50));
+	pais varchar(50),
+        primary key(id));
 
 INSERT INTO empleados (nombre, apellido, email, ciudad, pais) VALUES ('Cristina', 'Vazquez', 'cristina.vazquez@domain.com', 'Ourense', 'España');
 INSERT INTO empleados (nombre, apellido, email, ciudad, pais) VALUES ('Juan',     'García',  'juan.garcia@domain.com',      'Vigo',    'España');
