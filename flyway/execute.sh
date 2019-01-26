63524fcb8f79b990853075472056e95651a0a909
farinajf@gmail.com <farinajf@gmail.com>
2019-01-26 18:18:48 +0100

    flyway

---
+++ flyway/execute.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 1.- Crear imagen MYSQL
+../mysql/build.sh
+
+# 2.- Ejecutar flyway-mysql
+../mysql/run.sh
+
---
+++ mysql/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run --name=flyway-mysql -p 33060:3306 -d flyway-mysql
+
